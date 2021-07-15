3f1c18541a5c50dae54327cb18c2e85352622155
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-15 15:05:47 +0200

    Initial Commit

---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,19 @@
+//
+//  IncrementApp.swift
+//  Increment
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
---
+++ Increment/LandingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Increment
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+            .previewDevice("iPhone 11 Pro")
+    }
+}
